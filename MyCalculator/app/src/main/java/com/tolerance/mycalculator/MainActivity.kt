014f39155f8e82412c8a840a665909bb6d182fcc
Tolerance <zpgy77@gmail.com>
2021-11-24 22:05:20 +0900

    Adding decimal point function

--- MyCalculator/app/src/main/java/com/tolerance/mycalculator/MainActivity.kt
+++ MyCalculator/app/src/main/java/com/tolerance/mycalculator/MainActivity.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +22,20 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
 
     fun onClear(view: View) {
         tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+
     }
 }
