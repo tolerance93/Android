f982427032f82329d83b43777be6534ec535bea7
Tolerance <zpgy77@gmail.com>
2021-12-09 21:38:04 +0900

    displayMetrics and brushsize

--- KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/DrawingView.kt
+++ KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/DrawingView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -30,7 +31,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -87,6 +88,14 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         }
         invalidate()
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        // Consider DP. proportional to screen
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
