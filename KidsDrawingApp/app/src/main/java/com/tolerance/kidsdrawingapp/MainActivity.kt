341b082274ac1352922896baa2d3a14d6178e3bb
Tolerance <zpgy77@gmail.com>
2021-12-10 20:56:08 +0900

    Add color selection

--- KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/MainActivity.kt
@@ -4,9 +4,11 @@ import android.app.Dialog
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.dialog_brush_size.*
@@ -26,9 +28,7 @@ class MainActivity : AppCompatActivity() {
         val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
 
         mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
-        mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
-        )
+
 
         val ibBrush: ImageButton = findViewById(R.id.ib_brush)
         ibBrush.setOnClickListener{
@@ -60,5 +60,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         brushDialog.show()
+    }
+
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
     }
 }
