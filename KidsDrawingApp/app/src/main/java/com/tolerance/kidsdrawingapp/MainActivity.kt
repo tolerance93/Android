27497a600c2f03a0a715c31af493850966867e0b
Tolerance <zpgy77@gmail.com>
2021-12-15 21:46:09 +0900

    Select Image from Gallery

--- KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/MainActivity.kt
@@ -3,10 +3,13 @@ package com.tolerance.kidsdrawingapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -21,6 +24,14 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
 
+    val openGalleryLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+        if (result.resultCode == RESULT_OK && result.data != null) {
+            val imageBackGround: ImageView = findViewById(R.id.iv_background)
+            imageBackGround.setImageURI(result.data?.data)
+        }
+    }
+
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
             permissions ->
@@ -34,6 +45,10 @@ class MainActivity : AppCompatActivity() {
                         "Permission granted now you can read the storage files.",
                         Toast.LENGTH_LONG
                     ).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+
                 } else {
                     if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
                         Toast.makeText(
