62d8b71bfaef4cde15a361b1f3090a15084d4997
Tolerance <zpgy77@gmail.com>
2021-12-16 21:30:51 +0900

    Adding a provider

--- KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/tolerance/kidsdrawingapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -146,6 +149,22 @@ class MainActivity : AppCompatActivity() {
             ))
         }
     }
+
+    private fun getBitmapFromView(view: View) : Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return returnedBitmap
+    }
+
 
     private fun showRationaleDialog(
         title: String,
