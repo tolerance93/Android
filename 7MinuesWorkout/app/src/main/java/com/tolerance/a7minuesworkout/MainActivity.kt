e8b2626825ae61b5374c6078638aa3ce09c47e92
Tolerance <zpgy77@gmail.com>
2021-12-23 22:41:05 +0900

    View binding

--- 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/MainActivity.kt
+++ 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/MainActivity.kt
@@ -4,19 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.tolerance.a7minuesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "Clicked",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
