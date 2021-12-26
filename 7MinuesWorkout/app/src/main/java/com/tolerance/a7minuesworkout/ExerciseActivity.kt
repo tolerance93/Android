a11ddbfe9aa35abf45041abb83dacd96d59bd950
Tolerance <zpgy77@gmail.com>
2021-12-26 20:38:21 +0900

    Custom toolbar and back button

---
+++ 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/ExerciseActivity.kt
@@ -0,0 +1,27 @@
+package com.tolerance.a7minuesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tolerance.a7minuesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+
+    private var binding : ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        // add back button
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/MainActivity.kt
+++ 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tolerance.a7minuesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -17,11 +18,8 @@ class MainActivity : AppCompatActivity() {
 
         //val flStartButton : FrameLayout = findViewById(R.id.flStart)
         binding?.flStart?.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Clicked",
-                Toast.LENGTH_SHORT
-            ).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
 
