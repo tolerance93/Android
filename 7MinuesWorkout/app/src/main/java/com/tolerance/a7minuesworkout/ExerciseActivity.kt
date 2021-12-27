f99ba4901678426cddf46b874835d782c8532763
Tolerance <zpgy77@gmail.com>
2021-12-27 22:23:52 +0900

    Progressbar with timer

--- 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/ExerciseActivity.kt
+++ 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/ExerciseActivity.kt
@@ -2,11 +2,15 @@ package com.tolerance.a7minuesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.tolerance.a7minuesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding : ActivityExerciseBinding? = null
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,38 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+        setupRestView()
+    }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Timer finished", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
