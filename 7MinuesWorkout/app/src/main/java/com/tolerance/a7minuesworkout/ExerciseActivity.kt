111ff05a6f8918254b7c88422d7c865552234214
Tolerance <zpgy77@gmail.com>
2022-01-03 22:47:02 +0900

    Adding model constants

--- 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/ExerciseActivity.kt
+++ 7MinuesWorkout/app/src/main/java/com/tolerance/a7minuesworkout/ExerciseActivity.kt
@@ -16,6 +16,9 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
 
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -27,6 +30,8 @@ class ExerciseActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
+
+        exerciseList = Constants.defaultExerciseList()
 
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
@@ -66,6 +71,7 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                currentExercisePosition++
                 setupExerciseView()
             }
         }.start()
